80e6709da64f5440a09eb2fe37b22e9112239d71
marinatsuk <73252923+marinatsuk@users.noreply.github.com>
2021-12-29 11:20:16 +0200

    Add files via upload

--- get-properties.php
+++ get-properties.php
@@ -1,5 +1,5 @@
 <?php   
-    $response = file_get_contents("style.json");
+    $response = file_get_contents("config.json");
 
     header('Access-Control-Allow-Origin: *');
     header('Content-type: application/json; charset=utf-8');  
